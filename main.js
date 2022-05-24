63c76a6c2436e153547ee7abdc23df2012e30e93
nalbam <me@nalbam.com>
2022-05-24 13:09:25 +0900

    1.3.0 flag image

--- main.js
+++ main.js
@@ -173,12 +173,13 @@ chrome.storage.local.get('config', (c) => {
 
     // region flag
     if (config['flag'] !== 'disabled') {
-      document.querySelector('[data-testid="awsc-nav-regions-menu-button"]').insertAdjacentHTML("beforeBegin", `<span style='font-size:1.8em;line-height:1em;margin-right:0.2em'>${colors[region]['flag']}</span>`);
+      const img = `https://nalbam.github.io/aws-navbar-extension/flags/flag-${colors[region]['country']}.png`;
+      document.querySelector('[data-testid="awsc-nav-regions-menu-button"]').insertAdjacentHTML("beforeBegin", `<span style='line-height:1.5em;margin-right:0.3em;'><img src="${img}" style="width:20px;height:20px;"></span>`);
     }
   }
 
   // account info
   if (config['info'] !== undefined && config['info'][account_id] !== undefined) {
-    document.querySelector('[data-testid="awsc-nav-account-menu-button"]').insertAdjacentHTML("beforeBegin", `<span style='font-size:1.8em;line-height:1em;margin-right:0.2em'>${config['info'][account_id]}</span>`);
+    document.querySelector('[data-testid="awsc-nav-account-menu-button"]').insertAdjacentHTML("beforeBegin", `<span style='font-size:1.8em;margin-right:0.2em;'>${config['info'][account_id]}</span>`);
   }
 });
