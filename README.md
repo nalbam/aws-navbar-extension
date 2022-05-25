22d698735c2b89352bbbc939c133353ec718f476
nalbam <me@nalbam.com>
2022-05-25 14:01:20 +0900

    1.3.2

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 ## Install
 
 * [Chrome Web Store](https://chrome.google.com/webstore/detail/aws-colorful-navbar/kgifmgnlchjjippdpkblbdlfidcpceme)
+* [Microsoft Edge](https://chrome.google.com/webstore/detail/aws-colorful-navbar/kgifmgnlchjjippdpkblbdlfidcpceme)
+* [Mozilla Firefox](https://addons.mozilla.org/ko/firefox/addon/aws-colorful-navbar/)
 
 ## How to load
 
@@ -12,7 +14,9 @@
 * Enable `Developer Mode` by clicking the toggle switch
 * Click the `LOAD UNPACKED` button and select the extension directory
 
-![screenshot](images/screenshot.png)
+## Screenshots
+
+![aws navbar](images/screenshot.png)
 
 ![before](images/aws-navbar-extension-00.png)
 
--- main.js
+++ main.js
@@ -173,8 +173,8 @@ chrome.storage.local.get('config', (c) => {
 
     // region flag
     if (config['flag'] !== 'disabled') {
-      const img = `https://nalbam.github.io/aws-navbar-extension/flags/flag-${colors[region]['country']}.png`;
-      document.querySelector('[data-testid="awsc-nav-regions-menu-button"]').insertAdjacentHTML("beforeBegin", `<span style="line-height: 0;margin-right:0.5em;"><img src="${img}" style="width:20px;height:20px;"></span>`);
+      const flag = `https://nalbam.github.io/aws-navbar-extension/flags/flag-${colors[region]['country']}.png`;
+      document.querySelector('[data-testid="awsc-nav-regions-menu-button"]').insertAdjacentHTML("beforeBegin", `<span style="line-height:0;margin-right:0.5em;"><img src="${flag}" style="width:20px;height:20px;"></span>`);
       // document.querySelector('[data-testid="awsc-nav-regions-menu-button"]').insertAdjacentHTML("beforeBegin", `<span style="font-size:1.8em;margin-right:0.2em;">${colors[region]['emoji']}</span>`);
     }
   }
