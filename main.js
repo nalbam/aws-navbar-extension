be524fb3f1fbf6519786de839e524c9d201c8f16
nalbam <me@nalbam.com>
2022-05-25 21:22:29 +0900

    1.4.1

--- main.js
+++ main.js
@@ -1,115 +1,115 @@
 const colors = {
-  'N. Virginia': {
+  'us-east-1': {
     'country': 'united-states',
     'background': 'linear-gradient(to right, #00009b, #e6194b)',
     'emoji': '🇺🇸'
   },
-  'Ohio': {
+  'us-east-2': {
     'country': 'united-states',
     'background': 'linear-gradient(to right, #00009b, #3cb44b)',
     'emoji': '🇺🇸'
   },
-  'N. California': {
+  'us-west-1': {
     'country': 'united-states',
     'background': 'linear-gradient(to right, #00009b, #e58e00)',
     'emoji': '🇺🇸'
   },
-  'Oregon': {
+  'us-west-2': {
     'country': 'united-states',
     'background': 'linear-gradient(to right, #00009b, #4363d8)',
     'emoji': '🇺🇸'
   },
-  'Cape Town': {
+  'af-south-1': {
     'country': 'south-africa',
     'background': 'linear-gradient(to right, #9d6100, #d60000, #008a67, #273790)',
     'emoji': '🇿🇦'
   },
-  'Hong Kong': {
+  'ap-east-1': {
     'country': 'hong-kong',
     'background': 'linear-gradient(to right, #c9001a, #fcaeb6, #c9001a)',
     'emoji': '🇭🇰'
   },
-  'Jakarta': {
+  'ap-southeast-3': {
     'country': 'indonesia',
     'background': 'linear-gradient(to right, #f50003, #e3e2e2)',
     'emoji': '🇮🇩'
   },
-  'Mumbai': {
+  'ap-south-1': {
     'country': 'india',
     'background': 'linear-gradient(to right, #f98000, #c1bbe5, #009200)',
     'emoji': '🇮🇳'
   },
-  'Osaka': {
+  'ap-northeast-3': {
     'country': 'japan',
     'background': 'linear-gradient(to right, #d8d8d8, #d70048, #b87b00)',
     'emoji': '🇯🇵'
   },
-  'Seoul': {
+  'ap-northeast-2': {
     'country': 'south-korea',
     'background': 'linear-gradient(to right, #e30423, #00009b)',
     'emoji': '🇰🇷'
   },
-  'Singapore': {
+  'ap-southeast-1': {
     'country': 'singapore',
     'background': 'linear-gradient(to right, #e7b2b5, #ff3047, #ccc8a0)',
     'emoji': '🇸🇬'
   },
-  'Sydney': {
+  'ap-southeast-2': {
     'country': 'australia',
     'background': 'linear-gradient(to right, #00205b, #ef3340, #000080)',
     'emoji': '🇦🇺'
   },
-  'Tokyo': {
+  'ap-northeast-1': {
     'country': 'japan',
     'background': 'linear-gradient(to right, #d8d8d8, #d70048, #dfdfdf)',
     'emoji': '🇯🇵'
   },
-  'Central': {
+  'ca-central-1': {
     'country': 'canada',
     'background': 'linear-gradient(to right, #f60000, #ffdfe0, #f60000)',
     'emoji': '🇨🇦'
   },
-  'Frankfurt': {
+  'eu-central-1': {
     'country': 'germany',
     'background': 'linear-gradient(to right, #181d1d, #eb3d00, #dd9400)',
     'emoji': '🇩🇪'
   },
-  'Ireland': {
+  'eu-west-1': {
     'country': 'ireland',
     'background': 'linear-gradient(to right, #009555, #aaaaaa, #ec6b00)',
     'emoji': '🇮🇪'
   },
-  'London': {
+  'eu-west-2': {
     'country': 'united-kingdom',
     'background': 'linear-gradient(to right, #194171, #ef3340, #075aaa)',
     'emoji': '🇬🇧'
   },
-  'Milan': {
+  'eu-south-1': {
     'country': 'italy',
     'background': 'linear-gradient(to right, #008639, #aaaaaa, #c80d2e)',
     'emoji': '🇮🇹'
   },
-  'Paris': {
+  'eu-west-3': {
     'country': 'france',
     'background': 'linear-gradient(to right, #00569b, #aaaaaa, #f31c21)',
     'emoji': '🇫🇷'
   },
-  'Stockholm': {
+  'eu-north-1': {
     'country': 'sweden',
     'background': 'linear-gradient(to right, #004ca8, #fccc00, #004ca8)',
     'emoji': '🇸🇪'
   },
-  'Bahrain': {
+  'me-south-1': {
     'country': 'bahrain',
     'background': 'linear-gradient(to right, #9ea0a1, #e70e3b, #9e112b)',
     'emoji': '🇧🇭'
   },
-  'São Paulo': {
+  'sa-east-1': {
     'country': 'brazil',
     'background': 'linear-gradient(to right, #009a4a, #fed500, #009042)',
     'emoji': '🇧🇷'
   },
-  'Global': {
+  'global': {
     'country': 'global',
     'background': 'linear-gradient(to right, #0575e6, #159957)',
     'emoji': '🌍'
@@ -118,49 +118,46 @@ const colors = {
 
 const langs = {
   'ko': {
-    '버지니아 북부': 'N. Virginia',
-    '오하이오': 'Ohio',
-    '캘리포니아': 'N. California',
-    '오레곤': 'Oregon',
-    '케이프타운': 'Cape Town',
-    '홍콩': 'Hong Kong',
-    '자카르타': 'Jakarta',
-    '뭄바이': 'Mumbai',
-    '오사카': 'Osaka',
-    '서울': 'Seoul',
-    '싱가포르': 'Singapore',
-    '시드니': 'Sydney',
-    '도쿄': 'Tokyo',
-    '중부': 'Central',
-    '프랑크푸르트': 'Frankfurt',
-    '아일랜드': 'Ireland',
-    '런던': 'London',
-    '밀라노': 'Milan',
-    '파리': 'Paris',
-    '스톡홀름': 'Stockholm',
-    '바레인': 'Bahrain',
-    '상파울루': 'São Paulo',
     '글로벌': 'Global',
   }
 }
 
-const isDebug = false;
+const isDebug = true;
 
 // load
 chrome.storage.local.get('config', (c) => {
   const config = c.config !== undefined ? c.config : {};
   if (isDebug) console.log(`config: ${JSON.stringify(config, null, 2)}`);
 
-  // region
-  let region = document.querySelector('[data-testid="awsc-nav-regions-menu-button"]>span').innerText;
-  if (isDebug) console.log(`region: ${region}`);
+  // city
+  let city = document.querySelector('[data-testid="awsc-nav-regions-menu-button"]>span').innerText;
 
   // lang
   const lang = document.documentElement.lang;
-  if (langs.hasOwnProperty(lang) && langs[lang].hasOwnProperty(region)) {
-    region = langs[lang][region];
-    if (isDebug) console.log(`region: ${region}`);
+  if (langs.hasOwnProperty(lang) && langs[lang].hasOwnProperty(city)) {
+    city = langs[lang][city];
+    if (isDebug) console.log(`city: ${city}`);
   }
+
+  // aws service
+  let region = undefined;
+  let svc = undefined;
+  const re = /^https:\/\/([a-z0-9-]+)?(?:\.)?console\.aws\.amazon\.com\/([a-z0-9-]+)\/([a-z0-9]+(?=\/))?.*/;
+  const m = re.exec(window.location.href);
+  if (m !== undefined && m.length > 2) {
+    if (city === 'Global') {
+      region = 'global';
+    } else {
+      region = m[1];
+    }
+    svc = m[2];
+    if (svc === 'codesuite' && m.length > 3) {
+      svc = m[3];
+    }
+  }
+
+  if (isDebug) console.log(`region: ${region}`);
+  if (isDebug) console.log(`service: ${svc}`);
 
   if (colors.hasOwnProperty(region)) {
     // region header background
@@ -186,18 +183,6 @@ chrome.storage.local.get('config', (c) => {
     document.querySelector('[data-testid="awsc-nav-account-menu-button"]').insertAdjacentHTML("beforeBegin", `<span style="font-size:1.8em;margin-right:0.2em;">${config['info'][account_id]}</span>`);
   }
 
-  // aws service
-  let svc = undefined;
-  const re = /^https:\/\/([a-z0-9-]+)?(?:\.)?console\.aws\.amazon\.com\/([a-z0-9-]+)\/([a-z0-9]+(?=\/))?.*/;
-  const m = re.exec(window.location.href);
-  if (m !== undefined && m.length > 2) {
-    svc = m[2];
-    if (svc === 'codesuite' && m.length > 3) {
-      svc = m[3];
-    }
-    if (isDebug) console.log(`service: ${svc}`);
-  }
-
   // favicon
   if (config['favicon'] !== 'disabled' && svc !== undefined) {
     const x1 = document.querySelector("link[rel*='shortcut icon']");
@@ -208,7 +193,13 @@ chrome.storage.local.get('config', (c) => {
 
     const link = document.createElement('link');
     link.rel = 'icon';
-    link.href = `https://nalbam.github.io/aws-navbar-extension/svcs/${svc}.svg`;
+    if (svc === 'console'  ) {
+      link.href = `https://${region}.console.aws.amazon.com/favicon.ico`;
+    } else if (svc === 'billing') {
+      link.href = `https://us-east-1.console.aws.amazon.com/favicon.ico`;
+    } else {
+      link.href = `https://nalbam.github.io/aws-navbar-extension/svcs/${svc}.svg`;
+    }
     document.head.appendChild(link);
   }
 });
